b26094a734e2ea253c2bb2ab83e94b4686e294bd
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 19:44:21 +0330

    Checking_Numbers

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
-// let b = "wooh" + "oo".repeat(50);
-// let b = "wooh" + " ".repeat(50) + "oo";
-// let b = `wooh${"oo".repeat(50)}`;
-// console.log(b);
+const addToCart = (item, number) => {
+  // return Number.isFinite(number);
+  return Number.isSafeInteger(number);
+};
 
-console.log("butterfly".startsWith("butter"));
-console.log("butterfly".endsWith("fly"));
-console.log("butterfly".includes("fly"));
-console.log("butterfly".includes("caterpillar"));
+// console.log(addToCart('shirt', 5));
+console.log(addToCart('shirt', Math.pow(2, 54)));
