86393b857c89807e1a74b241149aea2bfd50f4f8
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 12:05:21 +0330

    While_Loops

---
+++ example18.js
@@ -0,0 +1,6 @@
+var names = ["Frodo", "Sam", "Merry", "Pippin"];
+var i = 0;
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
---
+++ example19.js
@@ -0,0 +1,6 @@
+var names = ["Frodo", "Sam", "Merry", "Pippin"];
+var i = 0;
+do {
+  console.log(names[i]);
+  i++;
+} while (i < names.length);
