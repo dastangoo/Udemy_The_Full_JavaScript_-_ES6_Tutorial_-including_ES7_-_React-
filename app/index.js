7c13266e915ebfbaa1ef962e28207d7c5591f75d
dastangoo <hamed.dastangoo@gmail.com>
2018-03-17 01:57:51 +0330

    Private_Methods

--- app/index.js
+++ app/index.js
@@ -1,32 +1,26 @@
-// const addSuffix = (x) => {
-//   const concat = (y) => {
-//     return y + x;
-//   }
-//   return concat;
-// };
-// 
-// let add_ness = addSuffix("ness");
-// console.log(add_ness);
-// let h = add_ness("happi");
-// console.log(h);
-// 
-// let add_ful = addSuffix("ful");
-// console.log(add_ful);
-// let f = add_ful("fruit");
-// console.log(f);
-// 
+const budget = () => {
+  let balance = 0;
+  let changeBal = (val) => {
+    balance += val;
+  }
+  const deposit20 = () => changeBal(20);
+  const withdraw20 = () => changeBal(-20);
+  const check = () => balance;
+  
+  // return {
+  //   deposit20: deposit20,
+  //   withdraw20: withdraw20,
+  //   check: check
+  // };
+  return { deposit20, withdraw20, check };
+};
 
-// const product = (x) => {
-//   return y => {
-//     return y * x;
-//   };
-// };
+let wallet = budget();
+console.log(wallet);
+console.log(wallet.check());
+wallet.deposit20();
+console.log(wallet.check());
+console.log(wallet.balance);
+wallet.withdraw20();
+console.log(wallet.check());
 
-const product = x => y => y * x;
-
-console.log(product);
-let mult5 = product(5);
-console.log(mult5(3));
-
-let double = product(2);
-console.log(double(3));
