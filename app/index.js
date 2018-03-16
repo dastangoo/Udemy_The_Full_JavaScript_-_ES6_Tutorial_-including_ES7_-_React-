529783305c0110d2e7b4421d485dc676e07dd168
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 22:09:59 +0330

    Inheriting_Classes

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
-class Animal {
-  constructor(name, height) {
-    this.name = name;
-    this.height = height;
+import Animal from './animal.js';
+
+class Lion extends Animal {
+  constructor(name, height, color) {
+    super(name, height);
+    this.color = color;
   }
   
   hello() {
-    console.log(`Hi! I'm ${this.name}`);
+    console.log(`Hi! I'm ${this.name} from Pride Rock!`);
   }
 }
 
-let king = new Animal("Mufasa", 4.5);
-console.log(king);
-king.hello();
+let son = new Lion("Simba", 2, "golden");
+console.log(son);
+son.hello();
