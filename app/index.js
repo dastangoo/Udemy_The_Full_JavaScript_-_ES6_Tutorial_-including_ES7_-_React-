2c9b8d8f94252cca93a57bc97d36aa9c9ac4d45c
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 19:23:14 +0330

    Arrow_Functions

--- app/index.js
+++ app/index.js
@@ -1,34 +1,20 @@
-let z = [4, 5, 6];
-// let four = z[0];
-// let five = z[1];
-// let six = z[2];
+// function cheer() {
+//   console.log("Woohoo!");
+// }
 
-// Destructuring Array
-// let [four, five, six] = z;
-// console.log(four, five, six);
+// var cheer = function() {
+//   console.log("Woohoo!");
+// }
 
-// let animals = ["Simba", "Zazu", "Ed"];
-// let [lion, bird] = animals;
-// console.log(lion, bird);
+// setTimeout(() => {
+//   console.log("Woohoo!");
+// }, 3000);
+// setInterval(function() {
+//   console.log("Woohoo!");
+// }, 3000);
 
-
-// Destructuring Object
-let king = {
-  name: 'Mufasa',
-  kids: 1 
-}
-
-// let name = king.name;
-// let kids = king.kids;
-
-// let {name, kids} = king;
-// console.log(name, kids);
-
-let son = {
-  name: 'Simba',
-  parents: 2
+let cheer = () => {
+  console.log("Woohoo!");
 };
-let name, parents;
-({name, parents} = son);
-// let {name, parents} = son;
-console.log(name, parents);
+
+cheer();
