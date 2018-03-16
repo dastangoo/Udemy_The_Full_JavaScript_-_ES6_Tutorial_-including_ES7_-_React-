736eeb5a29282f580d40e53488b816d717947e9b
dastangoo <hamed.dastangoo@gmail.com>
2018-03-17 01:45:48 +0330

    Function_Factories

--- app/index.js
+++ app/index.js
@@ -1,13 +1,32 @@
-let call = () => {
-  let secret = 'ES6 rocks!';
-  let reveal = () => {
-    console.log(secret);    
-  };
-  // reveal();
-  return reveal;
-};
+// const addSuffix = (x) => {
+//   const concat = (y) => {
+//     return y + x;
+//   }
+//   return concat;
+// };
+// 
+// let add_ness = addSuffix("ness");
+// console.log(add_ness);
+// let h = add_ness("happi");
+// console.log(h);
+// 
+// let add_ful = addSuffix("ful");
+// console.log(add_ful);
+// let f = add_ful("fruit");
+// console.log(f);
+// 
 
-// console.log(secret);
-// call();
-let unveil = call();
-unveil();
+// const product = (x) => {
+//   return y => {
+//     return y * x;
+//   };
+// };
+
+const product = x => y => y * x;
+
+console.log(product);
+let mult5 = product(5);
+console.log(mult5(3));
+
+let double = product(2);
+console.log(double(3));
