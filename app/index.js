024f38df74e19bd20ef84cf011bad3c66b3405c3
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 19:33:38 +0330

    Some tweaks

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
-let values = [20, 30, 40];
+// let values = [20, 30, 40];
+// var values = [20, 30, 40];
 
 // let double = (n) => {
 //   return n * 2;
@@ -11,11 +12,19 @@ let values = [20, 30, 40];
 //   return n * 2;
 // });
 // let doubled = values.map((n) => n * 2);
+// var doubled = values.map(function(n) {
+//   return n * 2;
+// })
 // console.log(doubled);
 
 let points = [7, 16, 21, 4, 3, 22, 5];
+// var points = [7, 16, 21, 4, 3, 22, 5];
 // let highScores = points.filter((n) => {
 //   return n > 15;
 // });
 let highScores = points.filter((n) => n > 15);
+// var highScores = points.filter(function(n) {
+//   return n > 15;
+// });
 console.log(highScores);
+
