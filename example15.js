958a4223a13096af2529627553a95c02b8cb5443
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 11:54:04 +0330

    Switch_Statements

---
+++ example15.js
@@ -0,0 +1,16 @@
+var x = 3;
+
+switch (x) {
+  case 1:
+    console.log("The number is one!");
+    break;
+  case 2:
+    console.log("The number is two!");
+    break;
+  case 3:
+    console.log("The number is three!");
+    break;
+  default:
+    console.log("The number is " + x);
+    break;
+}
