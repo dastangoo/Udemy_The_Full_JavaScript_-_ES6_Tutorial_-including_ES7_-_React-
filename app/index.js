3e9adc92a9a2f3e200145a1b11fa0193fd55d5b0
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 18:56:02 +0330

    Destructuring_Assignment_on_Arrays_and_Objects

--- app/index.js
+++ app/index.js
@@ -1,15 +1,34 @@
-// let a = [20, 30, 40];
-// let b = [10, 50];
-// let b = [10, ...a, 50];
-// console.log(b);
+let z = [4, 5, 6];
+// let four = z[0];
+// let five = z[1];
+// let six = z[2];
 
-// let a = ['Dana', 'Erik', 'Frank'];
-// let b = ['Alice', 'Bob', 'Carl', ...a];
-// console.log(b);
+// Destructuring Array
+// let [four, five, six] = z;
+// console.log(four, five, six);
 
-function collect(...a) {
-  console.log(a);
+// let animals = ["Simba", "Zazu", "Ed"];
+// let [lion, bird] = animals;
+// console.log(lion, bird);
+
+
+// Destructuring Object
+let king = {
+  name: 'Mufasa',
+  kids: 1 
 }
 
-// collect(1, 2, 3, 4, 5);
-collect(3, 6, 9, 12, 15);
+// let name = king.name;
+// let kids = king.kids;
+
+// let {name, kids} = king;
+// console.log(name, kids);
+
+let son = {
+  name: 'Simba',
+  parents: 2
+};
+let name, parents;
+({name, parents} = son);
+// let {name, parents} = son;
+console.log(name, parents);
