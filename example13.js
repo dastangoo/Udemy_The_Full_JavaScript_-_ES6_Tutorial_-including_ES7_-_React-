d7f6692cbfa996beb4766c885816ab15ca66e45e
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 11:51:24 +0330

    If_Statements

---
+++ example13.js
@@ -0,0 +1,9 @@
+var x = 10;
+
+if (8 > x) {
+  console.log('8 is greater!');
+} else if (15 > x){
+  console.log('15 is greater!');
+} else {
+  console.log('Not greater!');
+}
---
+++ example14.js
@@ -0,0 +1,10 @@
+var check = function(number) {
+  if (number % 2 == 0) {
+    console.log(number + " is even.");
+  } else {
+    console.log(number + " is odd.");
+  }
+}
+
+check(10);
+check(11);
