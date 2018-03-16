ea921864e6b2808a426f210b5289f8cb88637243
dastangoo <hamed.dastangoo@gmail.com>
2018-03-16 18:28:34 +0330

    Assignment_with_Let_and_Const

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World! (from webpack) with automatic updates!');
+// let limit = 100;
+// const limit = 100;
+// limit += 100;
+// limit = 200;
+// console.log(limit);
+
+const emails = [
+  'frodo@email.com',
+  'samwise@email.com',
+  'merry@email.com'
+];
+// emails = [];
+emails.push('pippin@email.com');
+
+console.log(emails);
