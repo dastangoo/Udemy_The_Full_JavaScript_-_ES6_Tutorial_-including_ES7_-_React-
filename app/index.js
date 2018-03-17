2d702e7dbc50978a0d57484733c2bbe1cf454468
dastangoo <hamed.dastangoo@gmail.com>
2018-03-17 10:33:00 +0330

    ES8_Proposal_-_Async

--- app/index.js
+++ app/index.js
@@ -1,11 +1,31 @@
-let obj = { a: 'one', b: 'two', c: 'three' };
-let keys = Object.keys(obj);
-let values = Object.values(obj);
-let entries = Object.entries(obj);
-console.log(keys);
-console.log(values);
-console.log(entries);
-
-for (let entry of entries) {
-  console.log(`key: ${entry[0]}, value: ${entry[1]}`);
+async function async_one() {
+  return "one";
 }
+
+async function async_two() {
+  return "two";
+} 
+
+// async function async_two() {
+//   throw new Error('Issue with async!');
+// }
+
+async function async_three() {
+  const one = await async_one();
+  console.log(one);
+  const two = await async_two();
+  console.log(two );
+}
+
+async function async_four() {
+  const [res_one, res_two] = await Promise.all(
+    [async_one(), async_two()]
+  );
+  
+  console.log(res_one, res_two);
+}
+// async_one().then(response => console.log(response));
+// async_two().catch(error => console.log(console.log(error)));
+
+// async_three();
+async_four();
