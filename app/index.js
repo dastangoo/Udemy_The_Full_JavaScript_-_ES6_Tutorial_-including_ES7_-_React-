980f48f1f2d4edb70d12abb0309956f9b8744167
dastangoo <hamed.dastangoo@gmail.com>
2018-03-17 09:36:59 +0330

    Some tweaks

--- app/index.js
+++ app/index.js
@@ -1,39 +1,9 @@
-// const arrayIterator = (array) => {
-//   let index = 0;
-// 
-//   return {
-//     next: () => {
-//       if (index < array.length) {
-//         let next = array[index];
-//         index += 1;
-//         return next;
-//       }
-//     }
-//   };
-// };
-// 
-// 
-// let it = arrayIterator([1, 2, 3]);
-// console.log(it.next());
-// console.log(it.next());
-// console.log(it.next());
-// console.log(it.next());
-
-// function* arrayIterator() {
-//   yield arguments;
-// }
-
-// function* arrayIterator() {
-//   for (let arg of arguments) {
-//     yield arg;
-//   }
-// }
-
-function* arrayIterator() {
-  yield* arguments;
-}
-
-var it = arrayIterator(1, 2, 3);
-console.log(it.next().value);
-console.log(it.next().value);
-console.log(it.next().value);
+let p = new Promise((resolve, reject) => {
+  // resolve('Resolved promise data');
+  // reject('Rejected promise data');
+  setTimeout(() => resolve('Resolved promise data'), 3000);
+});
+// console.log('after promise consumption');
+p.then(response => console.log(response))
+  .catch(error => console.log(error));
+console.log('after promise consumption');
