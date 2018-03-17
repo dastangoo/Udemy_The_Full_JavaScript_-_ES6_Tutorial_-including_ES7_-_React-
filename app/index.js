a1ea61933012d429b965d72728d240b63ca11ee9
dastangoo <hamed.dastangoo@gmail.com>
2018-03-17 10:07:38 +0330

    ES8_Proposal_-_More_Object_Manipulation

--- app/index.js
+++ app/index.js
@@ -1,15 +1,11 @@
-// In ES6
-// let a = Math.pow(2, 5);
+let obj = { a: 'one', b: 'two', c: 'three' };
+let keys = Object.keys(obj);
+let values = Object.values(obj);
+let entries = Object.entries(obj);
+console.log(keys);
+console.log(values);
+console.log(entries);
 
-// In ES7
-// let a = 2**5;
-// console.log(a);
-
-// In ES6
-// let b = "wonderful".includes("wonder");
-// console.log(b);
-
-// In ES7
-let b = [2, 3, 4, 5, 6].includes(4);
-console.log(b);
-
+for (let entry of entries) {
+  console.log(`key: ${entry[0]}, value: ${entry[1]}`);
+}
